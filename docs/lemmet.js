d2652b19ef7ced7b27640415715ff68c7a5660eb
wakimoto <wakimoto@sgnx.co.jp>
2018-10-03 22:43:22 +0900

    docs update

--- docs/lemmet.js
+++ docs/lemmet.js
@@ -1,38 +1,14 @@
 /**
-
-Lemmet記法
-
-div#page
-  div.logo
-  ul#navigation
-    li*5>a
-
-対応するEmmet記法
-div#page>div.logo+ul#navigation>li*5>a
- 
-HTML
-<div id="page">
-	<div class="logo"></div>
-	<ul id="navigation">
-		<li><a href=""></a></li>
-		<li><a href=""></a></li>
-		<li><a href=""></a></li>
-		<li><a href=""></a></li>
-		<li><a href=""></a></li>
-	</ul>
-</div>
-
- */
-
-/**
  * lemmet2emmet
- * @param {string} str 
+ * @param {string} str
  */
 function lemmet2emmet(str) {
     str = str.replace(/　/g, "\t")
     str = str.replace(/  /g, "\t")
+    str = str.replace(/\r/g, "")
     let strArray = str.split("\n")
     let last_indent_count = 0
+    let last_delimiter_count = 0
     const reg = strArray[0].match(/\t/g)
     if (reg!=null) {
         last_indent_count = reg.length
@@ -52,23 +28,30 @@ function lemmet2emmet(str) {
             delimiter = "+"
         else if (indent_count > last_indent_count)
             delimiter = ">"
-        else
-            delimiter = "^"
+        else {
+            let reverse_count = last_indent_count - indent_count + last_delimiter_count
+            for (let i = 0; i <= reverse_count - 1; i++){
+                delimiter += "^"
+            }
+        }
         emmetstr += delimiter + strArray[i].replace(/\t+/g,"")
         last_indent_count = indent_count
-
+        const reg2 = strArray[i].match(/>/g)
+        if (reg2!=null) {
+            last_delimiter_count = reg2.length
+        }
     }
     return emmetstr
 }
 
 /**
  * emmet2heml
- * 
+ *
  * WIP
- * 
+ *
  * Call external function?
- * 
- * @param {string} str 
+ *
+ * @param {string} str
  */
 function emmet2html(str) {
     return str;
